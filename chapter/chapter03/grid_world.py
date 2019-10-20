98c4d7bcf341eed8ac647ae053639bb885425663
LemuriaChen <dandanlemuria@gmail.com>
2019-10-20 08:12:07 +0800

    add figure 3.5

--- chapter/chapter03/grid_world.py
+++ chapter/chapter03/grid_world.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 from scipy.linalg import solve
+from scipy.optimize import fsolve
 
 
 def bound_value(x, min_val=0, max_val=4):
@@ -17,6 +18,7 @@ fix_const = .9 / 4
 # solve Bellman equation for state value function (by solving linear equations)
 # there is also some other algorithms, but in order to understand Bellman equation, I use linear equations here.
 
+# bellman equation
 for i in range(grid_size):
     for j in range(grid_size):
         cof = np.zeros([grid_size, grid_size])
@@ -46,4 +48,62 @@ print('state value function of GridWorld under a random policy:')
 print(np.array(sol).reshape([grid_size, grid_size]).round(1))
 
 
+# bellman optimality equation
+def bellman_optimality_function(x, gamma=0.9):
+
+    # 0   1   2   3   4
+    # 5   6   7   8   9
+    # 10  11  12  13  14
+    # 15  16  17  18  19
+    # 20  21  22  23  24
+
+    # this function can be more automated instead of writing equations manually
+    # for the sake of understanding, we remain unchanged for the time being
+
+    return np.array([
+        x[0] - max(x[1] * gamma, x[5] * gamma, x[0] * gamma - 1, ),
+        x[1] - (x[21] * gamma + 10),
+        x[2] - max(x[1] * gamma, x[3] * gamma, x[7] * gamma, x[2] * gamma - 1, ),
+        x[3] - (x[13] * gamma + 5),
+        x[4] - max(x[3] * gamma, x[9] * gamma, x[4] * gamma - 1, ),
+        x[5] - max(x[0] * gamma, x[6] * gamma, x[10] * gamma, x[5] * gamma - 1, ),
+        x[6] - max(x[1] * gamma, x[5] * gamma, x[7] * gamma, x[11] * gamma, ),
+        x[7] - max(x[2] * gamma, x[6] * gamma, x[8] * gamma, x[12] * gamma, ),
+        x[8] - max(x[3] * gamma, x[7] * gamma, x[9] * gamma, x[13] * gamma, ),
+        x[9] - max(x[4] * gamma, x[8] * gamma, x[14] * gamma, x[9] * gamma - 1, ),
+        x[10] - max(x[5] * gamma, x[11] * gamma, x[15] * gamma, x[10] * gamma - 1, ),
+        x[11] - max(x[6] * gamma, x[10] * gamma, x[16] * gamma, x[12] * gamma, ),
+        x[12] - max(x[7] * gamma, x[11] * gamma, x[17] * gamma, x[13] * gamma, ),
+        x[13] - max(x[8] * gamma, x[12] * gamma, x[18] * gamma, x[14] * gamma, ),
+        x[14] - max(x[9] * gamma, x[13] * gamma, x[19] * gamma, x[14] * gamma - 1, ),
+        x[15] - max(x[10] * gamma, x[20] * gamma, x[16] * gamma, x[15] * gamma - 1, ),
+        x[16] - max(x[11] * gamma, x[15] * gamma, x[21] * gamma, x[17] * gamma, ),
+        x[17] - max(x[12] * gamma, x[16] * gamma, x[22] * gamma, x[18] * gamma, ),
+        x[18] - max(x[13] * gamma, x[17] * gamma, x[23] * gamma, x[19] * gamma, ),
+        x[19] - max(x[14] * gamma, x[18] * gamma, x[24] * gamma, x[19] * gamma - 1, ),
+        x[20] - max(x[15] * gamma, x[21] * gamma, x[20] * gamma - 1, ),
+        x[21] - max(x[16] * gamma, x[20] * gamma, x[22] * gamma, x[21] * gamma - 1),
+        x[22] - max(x[17] * gamma, x[21] * gamma, x[23] * gamma, x[22] * gamma - 1),
+        x[23] - max(x[18] * gamma, x[22] * gamma, x[24] * gamma, x[23] * gamma - 1),
+        x[24] - max(x[19] * gamma, x[23] * gamma, x[24] * gamma - 1),
+    ])
+
+
+"""
+x = np.array([
+    22.0, 24.4, 22.0, 19.4, 17.5,
+    19.8, 22.0, 19.8, 17.8, 16.0,
+    17.8, 19.8, 17.8, 16.0, 14.4,
+    16.0, 17.8, 16.0, 14.4, 13.0,
+    14.4, 16.0, 14.4, 13.0, 11.7
+])
+print(bellman_optimality_function(x).reshape([grid_size, grid_size]).round(1))
+"""
+
+# solve non-linear equation
+sol = fsolve(bellman_optimality_function, x0=np.array([1]*grid_size*grid_size))
+
+# page 87, Figure 3.5
+print('\noptimal state value function of GridWorld:')
+print(np.array(sol).reshape([grid_size, grid_size]).round(1))
 
